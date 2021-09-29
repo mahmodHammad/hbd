57dff8a26afcdf11ab21158fd4139c9f383b306e
= <hodaman2012@gmail.com>
2021-09-30 00:06:02 +0200

    update

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'sep 30, 2020',
+  birthdate: 'sep 30, 2021',
   name: 'Sara'
 };
 
@@ -26,7 +26,7 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date(`${config.birthdate} 00:00:00`).getTime();
+let countDown = new Date(`${config.birthdate} 0:10:00`).getTime();
 x = setInterval(function() {
   let now = new Date().getTime(),
     distance = countDown - now;
